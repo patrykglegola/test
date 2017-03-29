360d169361cc31159f562e68460318d081c027e2
Patryk Glegoła <patryk.glegola@gmail.com>
2017-03-29 17:59:07 +0200

    Wstępne przygotowanie ListView i danych dla adaptera.

--- app/src/main/java/com/example/student/test1/SetGradesActivity.java
+++ app/src/main/java/com/example/student/test1/SetGradesActivity.java
@@ -3,13 +3,22 @@ package com.example.student.test1;
         import android.os.Bundle;
         import android.support.v7.app.AppCompatActivity;
         import android.widget.ArrayAdapter;
+        import android.widget.ListView;
 
 public class SetGradesActivity extends AppCompatActivity {
+
+    ListView setGradesList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_grades);
+        setGradesList = (ListView) findViewById(R.id.gradesList);
+        int amountOfGrades = getIntent().getIntExtra("amountOfGrades", 0);
+        int[] grade = new int[amountOfGrades];
+
+
     }
 
 }
